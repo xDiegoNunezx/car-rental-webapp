4075886e67c46bfd1a3632b9859891e5b1e69fa7
Diego Ignacio Núñez Hernández <diego.nunezh@ingenieria.unam.edu>
2025-05-12 08:29:10 -0600

    Project created

---
+++ proyecto_final/src/test/java/edu/unam/dgtic/proyecto_final/ProyectoFinalApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.unam.dgtic.proyecto_final;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoFinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
